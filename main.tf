defcb5a49213234039aa88721becd288e95e4fbe
pawelros <rosiu@outlook.com>
2019-04-25 22:39:19 +0200

    Fix SQL conn strings + add firewall rule

--- main.tf
+++ main.tf
@@ -54,6 +54,16 @@ resource "azurerm_sql_database" "etil-sql-database" {
   server_name         = "${azurerm_sql_server.sql-server.name}"
 }
 
+# The Azure feature Allow access to Azure services can be enabled by setting 
+# start_ip_address and end_ip_address to 0.0.0.0 which (is documented in the Azure API Docs).
+resource "azurerm_sql_firewall_rule" "sql-firewall" {
+  name                = "AllowAzureServices"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  server_name         = "${azurerm_sql_server.sql-server.name}"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
 # Azure Cache for Redis instance
 resource "azurerm_redis_cache" "redis" {
   name                = "${local.service_prefix}-${local.environment}",
@@ -69,11 +79,11 @@ resource "azurerm_redis_cache" "redis" {
 
 # Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = "${local.service_prefix}-${local.environment}",
+  name                     = "${local.service_prefix}${local.environment}",
   resource_group_name      = "${azurerm_resource_group.rg.name}",
   location                 = "${azurerm_resource_group.rg.location}",
-  account_replication_type = "",
-  account_tier             = "",
+  account_replication_type = "LRS",
+  account_tier             = "Standard",
 }
 
 # Application Insights
@@ -104,9 +114,9 @@ resource "azurerm_app_service" "as" {
   app_settings = {
       ApplicationInsights__InstrumentationKey = "${azurerm_application_insights.insights.instrumentation_key}",
       AzureStorage__ConnectionString          = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.storage.name};AccountKey=${azurerm_storage_account.storage.primary_access_key};EndpointSuffix=core.windows.net",
-      ConnectionStrings__FContext             = "${azurerm_sql_database.etil-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.etil-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
-      ConnectionStrings__IContext             = "${azurerm_sql_database.import-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.import-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
-      ConnectionStrings__RContext             = "${azurerm_redis_cache.redis.name}.redis.cache.windows.net:6380,password=${azurerm_redis_cache.redis.primary_access_key},ssl=True,abortConnect=False,allowAdmin=true",
-      ConnectionStrings__AContext             = "${azurerm_sql_database.auth-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.auth-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
+      ConnectionStrings__FContext             = "Server=${azurerm_sql_database.etil-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.etil-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
+      ConnectionStrings__IContext             = "Server=${azurerm_sql_database.import-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.import-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
+      ConnectionStrings__RContext             = "Server=${azurerm_redis_cache.redis.name}.redis.cache.windows.net:6380,password=${azurerm_redis_cache.redis.primary_access_key},ssl=True,abortConnect=False,allowAdmin=true",
+      ConnectionStrings__AContext             = "Server=${azurerm_sql_database.auth-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.auth-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
   }
 }
