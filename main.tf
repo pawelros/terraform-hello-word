ce2f06f220ac4ee9fac6c4a63bb0434bba538c1f
pawelros <rosiu@outlook.com>
2019-04-25 19:21:32 +0200

    Add Redis cache

--- main.tf
+++ main.tf
@@ -54,6 +54,19 @@ resource "azurerm_sql_database" "etil-sql-database" {
   server_name         = "${azurerm_sql_server.sql-server.name}"
 }
 
+# Create Azure Cache for Redis instance
+resource "azurerm_redis_cache" "redis" {
+  name                = "${local.service_prefix}-${local.environment}",
+  location            = "${azurerm_resource_group.rg.location}",
+  resource_group_name = "${azurerm_resource_group.rg.name}",
+  capacity            = 0,
+  family              = "C",
+  sku_name            = "Basic",
+  enable_non_ssl_port = false,
+  redis_configuration = {
+  }
+}
+
 # Create an app service plan
 resource "azurerm_app_service_plan" "sp" {
   name                = "app-service-plan"
@@ -74,7 +87,7 @@ resource "azurerm_app_service" "as" {
   app_settings = {
       ConnectionStrings__FContext = "${azurerm_sql_database.etil-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.etil-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
       ConnectionStrings__IContext = "${azurerm_sql_database.import-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.import-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
-      ConnectionStrings__RContext = "",
+      ConnectionStrings__RContext = "${azurerm_redis_cache.redis.name}.redis.cache.windows.net:6380,password=${azurerm_redis_cache.redis.primary_access_key},ssl=True,abortConnect=False,allowAdmin=true",
       ConnectionStrings__AContext = "${azurerm_sql_database.auth-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.auth-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
   }
 }
