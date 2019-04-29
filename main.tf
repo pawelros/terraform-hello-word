89e9bdbc21f19aeb1bc3d80b9fbe640c6dc3082f
pawelros <rosiu@outlook.com>
2019-04-29 14:54:58 +0200

    Use var file instead of locals

--- main.tf
+++ main.tf
@@ -3,16 +3,9 @@ provider "azurerm" {
 version = "=1.25.0"
 }
 
-# Local variables
-locals {
-  service_prefix = "etil"
-  environment = "test",
-  sql_admin_username = "etil_admin"
-}
-
 # Resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "${local.environment}"
+    name     = "${var.environment}"
     location = "West Europe"
 }
 
@@ -25,30 +18,30 @@ resource "random_string" "password" {
 }
 
 resource "azurerm_sql_server" "sql-server" {
-  name                         = "${local.service_prefix}-${local.environment}"
+  name                         = "${var.service_prefix}-${var.environment}"
   resource_group_name          = "${azurerm_resource_group.rg.name}"
   location                     = "${azurerm_resource_group.rg.location}"
   version                      = "12.0"
-  administrator_login          = "${local.sql_admin_username}"
+  administrator_login          = "${var.sql_admin_username}"
   administrator_login_password = "${random_string.password.result}"
 }
 
 resource "azurerm_sql_database" "auth-sql-database" {
-  name                = "auth-${local.service_prefix}-${local.environment}"
+  name                = "auth-${var.service_prefix}-${var.environment}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   location            = "${azurerm_resource_group.rg.location}"
   server_name         = "${azurerm_sql_server.sql-server.name}"
 }
 
 resource "azurerm_sql_database" "import-sql-database" {
-  name                = "import-${local.service_prefix}-${local.environment}"
+  name                = "import-${var.service_prefix}-${var.environment}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   location            = "${azurerm_resource_group.rg.location}"
   server_name         = "${azurerm_sql_server.sql-server.name}"
 }
 
 resource "azurerm_sql_database" "etil-sql-database" {
-  name                = "${local.service_prefix}-${local.environment}"
+  name                = "${var.service_prefix}-${var.environment}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   location            = "${azurerm_resource_group.rg.location}"
   server_name         = "${azurerm_sql_server.sql-server.name}"
@@ -66,7 +59,7 @@ resource "azurerm_sql_firewall_rule" "sql-firewall" {
 
 # Azure Cache for Redis instance
 resource "azurerm_redis_cache" "redis" {
-  name                = "${local.service_prefix}-${local.environment}",
+  name                = "${var.service_prefix}-${var.environment}",
   location            = "${azurerm_resource_group.rg.location}",
   resource_group_name = "${azurerm_resource_group.rg.name}",
   capacity            = 0,
@@ -79,7 +72,7 @@ resource "azurerm_redis_cache" "redis" {
 
 # Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = "${local.service_prefix}${local.environment}",
+  name                     = "${var.service_prefix}${var.environment}",
   resource_group_name      = "${azurerm_resource_group.rg.name}",
   location                 = "${azurerm_resource_group.rg.location}",
   account_replication_type = "LRS",
@@ -88,7 +81,7 @@ resource "azurerm_storage_account" "storage" {
 
 # Application Insights
 resource "azurerm_application_insights" "insights" {
-  name                = "${local.service_prefix}-${local.environment}",
+  name                = "${var.service_prefix}-${var.environment}",
   resource_group_name = "${azurerm_resource_group.rg.name}",
   location            = "${azurerm_resource_group.rg.location}",
   application_type    = "web"
@@ -107,16 +100,16 @@ resource "azurerm_app_service_plan" "sp" {
 
 # Create an app service
 resource "azurerm_app_service" "as" {
-  name                  = "${local.service_prefix}-${local.environment}"
+  name                  = "${var.service_prefix}-${var.environment}"
   resource_group_name   = "${azurerm_resource_group.rg.name}",
   app_service_plan_id   = "${azurerm_app_service_plan.sp.id}",
   location              = "${azurerm_resource_group.rg.location}",
   app_settings = {
       ApplicationInsights__InstrumentationKey = "${azurerm_application_insights.insights.instrumentation_key}",
       AzureStorage__ConnectionString          = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.storage.name};AccountKey=${azurerm_storage_account.storage.primary_access_key};EndpointSuffix=core.windows.net",
-      ConnectionStrings__FContext             = "Server=${azurerm_sql_database.etil-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.etil-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
-      ConnectionStrings__IContext             = "Server=${azurerm_sql_database.import-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.import-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
+      ConnectionStrings__FContext             = "Server=${azurerm_sql_database.etil-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.etil-sql-database.name};Persist Security Info=True;User ID=${var.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
+      ConnectionStrings__IContext             = "Server=${azurerm_sql_database.import-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.import-sql-database.name};Persist Security Info=True;User ID=${var.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
       ConnectionStrings__RContext             = "${azurerm_redis_cache.redis.name}.redis.cache.windows.net:6380,password=${azurerm_redis_cache.redis.primary_access_key},ssl=True,abortConnect=False,allowAdmin=true",
-      ConnectionStrings__AContext             = "Server=${azurerm_sql_database.auth-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.auth-sql-database.name};Persist Security Info=True;User ID=${local.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
+      ConnectionStrings__AContext             = "Server=${azurerm_sql_database.auth-sql-database.name}.database.windows.net;Initial Catalog=${azurerm_sql_database.auth-sql-database.name};Persist Security Info=True;User ID=${var.sql_admin_username};Password=${azurerm_sql_server.sql-server.administrator_login_password};MultipleActiveResultSets=True",
   }
 }
